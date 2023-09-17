30c62a78df2eb16f558472631784577d1e532179
laudy-15 <savannahlaudicina@gmail.com>
2023-09-17 14:54:25 -0400

    Update CircleApp.java
    Edited to adjust for our WordImage and HangmanImage classes

--- csc121-creative-project/src/CircleApp.java
+++ csc121-creative-project/src/CircleApp.java
@@ -5,23 +5,30 @@ import processing.event.*;
  * Provides the scaffolding to launch a Processing application
  */
 public class CircleApp extends PApplet {
-    CircleWorld w;
+    HangmanImage h;
+    WordImage w;
     
     public void settings() {
         this.size(400, 400);
+        //TO-DO w.size(70, 70); trying to change font size for words 
     }
     
     public void setup() {
-        w = new CircleWorld(200, 0);
+        h = new HangmanImage(0);
+        w = new WordImage("pearl", 0, 5);
     }
     
     public void draw() {
-        w = w.update();
-        w.draw(this);
+        h = h.update();
+    	h.draw(this);
+    	w = w.update();
+    	w.draw(this);
     }
     
     public void mousePressed(MouseEvent mev) {
-        w = w.mousePressed(mev);
+        //w = w.mousePressed(mev);
+        //h = h.mousePressed(mev);
+        
     }
     
     public void keyPressed(KeyEvent kev) {
