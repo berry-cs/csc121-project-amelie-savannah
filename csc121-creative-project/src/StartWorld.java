047271541238bba5a22d5d91ed00bac213951ada
laudy-15 <savannahlaudicina@gmail.com>
2023-09-29 13:49:07 -0400

    Create StartWorld.java

---
+++ csc121-creative-project/src/StartWorld.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+import processing.event.KeyEvent;
+
+public class StartWorld implements IWorld {
+
+	@Override
+	public PApplet draw(PApplet c) {
+		c.background(120);
+		c.text("Press space bar to start", 100, 100);
+		return c;
+	}
+
+	@Override
+	public IWorld update() {
+		return this;
+	}
+
+	@Override
+	public IWorld keyPressed(KeyEvent kev) {
+		if (kev.getKey() == ' ') {
+			return  new HangmanWorld();
+		} else {
+			return this;
+		}
+	}
+
+}
