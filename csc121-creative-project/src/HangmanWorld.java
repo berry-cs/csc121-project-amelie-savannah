01ebd094627501d86f9875bf935c262e7d32d9fd
laudy-15 <savannahlaudicina@gmail.com>
2023-09-29 13:48:54 -0400

    keyPressed got updated
    implemented IWorld, and updated the keyPressed() method that uses methods from the WordImage class to display the word

--- csc121-creative-project/src/HangmanWorld.java
+++ csc121-creative-project/src/HangmanWorld.java
@@ -1,7 +1,7 @@
 import processing.core.*;
 import processing.event.KeyEvent;
 
-public class HangmanWorld {
+public class HangmanWorld implements IWorld {
     HangmanImage h;
     WordImage w;
 
@@ -22,14 +22,29 @@ public class HangmanWorld {
         return c;
     }
     
-    public HangmanWorld update() {
+    public IWorld update() {
         return this;
     }
     
-    public HangmanWorld keyPressed(KeyEvent kev) {
-        char ch = Character.toLowerCase( kev.getKey() );
-       /* (ch >= 'a' &&   ch <= 'z')  */
-        return new HangmanWorld( h.update(), w.update() );
+    /** updates the world according to the key press */
+    public IWorld keyPressed(KeyEvent kev) {
+    	if (kev.getKey() == '.') {  //if the '.' is pressed, a new world is returned
+    		return new StartWorld();
+    	} else if (this.w.wordDone()) {  //if the word is already finished the world will not update
+    		return this;
+    	} else if (this.h.manFilled()) {  //if the hangman is already full the world will not update
+    		return this;
+    	} else {
+	        char ch = Character.toLowerCase( kev.getKey() );
+	       /* (ch >= 'a' &&   ch <= 'z')  */
+	        if (this.w.isInWord(ch)) {    //if the key pressed is a letter in the word:
+	        	this.w.makeGuess(ch);  //"make the guess" --> updates the wordImage
+	        	return new HangmanWorld( this.h, w.update() ); //returns HangmanWorld without updating the HangmanImage, but updates the WordImage
+	        } else {
+	        	this.w.status++;   //increases the WordImage status if the key press is not in the word(kinda bogus rn, not sure if we need it)
+	        	return new HangmanWorld( h.update(), w.update() );  //returns a HangmanWorld, updating the HangmanImage to account for the wrong guess
+	        }
+    	}
     }
 
 
