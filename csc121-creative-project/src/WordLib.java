646c656ac8327d211baa504e7356df812e765459
laudy-15 <savannahlaudicina@gmail.com>
2023-10-01 17:58:33 -0400

    added an array
    set the array of words to a list of strings to be randomly picked from. added a get and set method for the field

--- csc121-creative-project/src/WordLib.java
+++ csc121-creative-project/src/WordLib.java
@@ -3,13 +3,22 @@
  * correct letters guessed
  * number of blank spaces(length of the word) */
 public class WordLib {
-	int length;
-	String corLetters; //this will be a list later
-	String word;
+	private String[] words; //preset list of words to randomly select from
 	
-	public WordLib(int length, String corLetters, String word) {
-		this.length = 5;
-		this.corLetters = corLetters;
-		this.word = "pearl";
+	public WordLib() {
+		this.words = new String[]{"pearl", "government", "mess", "winner", "person", "superior", "deck", "engineer", "abundant", "irony", "utter", "archaic",
+				"dead", "premium", "permission", "attitude", "see", "baseball", "depart", "increase", "grimace", "manual", "teach", "match", "long", "delicate",
+				"vessel", "payment", "sink", "plead", "convince", "census", "conductor", "registration", "proper", "table", "pitch", "exchange", "landowner", "coup", "grace",
+				"growth", "earsplitting", "cabbageworm"};
 	}
+
+	public String[] getWords() {
+		return words;
+	}
+
+	public void setWords(String[] words) {
+		this.words = words;
+	}
+	
+	
 }
