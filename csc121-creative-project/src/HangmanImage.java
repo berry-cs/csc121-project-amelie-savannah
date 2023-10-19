7fc69557e77b4033f725f7a4c8e310abc242b784
laudy-15 <savannahlaudicina@gmail.com>
2023-10-19 13:48:20 -0400

    no more magic numbers

--- csc121-creative-project/src/HangmanImage.java
+++ csc121-creative-project/src/HangmanImage.java
@@ -8,6 +8,21 @@ import processing.event.MouseEvent;
 public class HangmanImage {
 	private int status; //number representing how many wrong guesses
 	
+	final static int BLEFTMARGIN = 200;
+	final static int STOPMARGIN = 150;
+	final static int BRIGHTMARGIN = 350;
+	final static int SLEFTMARGIN = 240;
+	final static int S2TOPMARGIN = 5;
+	final static int ARIGHTMARGIN = 300;
+	final static int START = 300;
+	final static int HRADIUS = 20;
+	final static int SBOTTOMMARGIN = 30;
+	final static int S3TOPMARGIN = 80;
+	final static int LABOTTOMMARGIN = 45;
+	final static int LARIGHTMARGIN = 280;
+	final static int RARIGHTMARGIN = 320;
+	final static int LTOPMARGIN = 105;
+	
 	public HangmanImage(int status) {
 		this.status = status;
 	}
@@ -18,32 +33,32 @@ public class HangmanImage {
     public PApplet draw(PApplet c) {
         c.textSize(20);
        // c.text("Hangman!", 20, 20);
-       c.fill(0, 0, 255);
+        c.fill(0, 0, 255);
         
         //stand of the hangman
-        c.line(200, 150, 350, 150); //base of the stand
-        c.line(240, 150, 240, 5); //height of the stand
-        c.line(240, 5, 300, 5);  //arm of the stand
+        c.line(BLEFTMARGIN, STOPMARGIN, BRIGHTMARGIN, STOPMARGIN); //base of the stand
+        c.line(SLEFTMARGIN, STOPMARGIN, SLEFTMARGIN, S2TOPMARGIN); //height of the stand
+        c.line(SLEFTMARGIN, S2TOPMARGIN, ARIGHTMARGIN, S2TOPMARGIN);  //arm of the stand
         
         
         //hangman dude
         if (this.status >= 1) {
-        	c.circle(300, 20, 20); //head
+        	c.circle(START, HRADIUS, HRADIUS); //head
         } 
         if (this.status >= 2) {
-        	c.line(300, 30, 300, 80);  //spine
+        	c.line(START, SBOTTOMMARGIN, START, S3TOPMARGIN);  //spine
         } 
         if (this.status >= 3) {
-        	c.line(300, 45, 280, 30); //left arm
+        	c.line(START, LABOTTOMMARGIN, LARIGHTMARGIN, SBOTTOMMARGIN); //left arm
         } 
         if (this.status >= 4) {
-        	c.line(300, 45, 320, 30); //right arm
+        	c.line(START, LABOTTOMMARGIN, RARIGHTMARGIN, SBOTTOMMARGIN); //right arm
         } 
         if (this.status >= 5) {
-        	c.line(300, 80, 280, 105); //left leg
+        	c.line(START, S3TOPMARGIN, LARIGHTMARGIN, LTOPMARGIN); //left leg
         } 
         if (this.status >= 6) {
-        	c.line(300, 80, 320, 105); //right leg
+        	c.line(START, S3TOPMARGIN, RARIGHTMARGIN, LTOPMARGIN); //right leg
         }
         
         return c;
