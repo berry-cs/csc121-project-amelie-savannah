a04db0959994c54e6f5158abc9c1eafa0a3fd025
strzy <happyth0ug5ts@gmail.com>
2023-10-20 10:20:55 -0400

    Removed magic numbers and changed "photo" to "start" to be more descriptive

--- csc121-creative-project/src/StartWorld.java
+++ csc121-creative-project/src/StartWorld.java
@@ -3,19 +3,22 @@ import processing.core.PImage;
 import processing.event.KeyEvent;
 
 public class StartWorld implements IWorld {
-	private PImage photo;
+	private PImage start; // title screen background image
+	
+	final static int IMAGEX = 0;
+	final static int IMAGEY = 0;
 	
 	public StartWorld() {
-		photo = null;
+		this.start = null;
 	}
 
 	@Override
 	public PApplet draw(PApplet c) {
-		if (photo == null) {
-			 photo = c.loadImage("hangman.png");
+		if (start == null) {
+			start = c.loadImage("hangman.png");
 		}
-		c.background(photo);
-		c.image(photo, 0, 0);
+		c.background(start);
+		c.image(start, IMAGEX, IMAGEY);
 		// c.background(120);
 		//c.text("Press space bar to start", 130, 150);
 		return c;
