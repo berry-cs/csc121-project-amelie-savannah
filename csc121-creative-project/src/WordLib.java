a9a4880b36189ae161aecd88b9316c5c39ebcacb
strzy <happyth0ug5ts@gmail.com>
2023-11-09 17:44:08 -0500

    Added file input/output
    Removed the original words array and replaced with a text file containing all the words to choose from.
    
    Added a private method to read files in from "words.txt".

--- csc121-creative-project/src/WordLib.java
+++ csc121-creative-project/src/WordLib.java
@@ -1,15 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
 
 /** word library that randomly selects a word, and holds the word data including
  * correct letters guessed
  * number of blank spaces(length of the word) */
 public class WordLib {
-	private String[] words; //preset list of words to randomly select from
+	private String[] words; // preset list of words to randomly select from
+	private Scanner sc; // Scanner to read the file
+	private File f; // File variable
+	private int num; // number of words in file
+	
 	
 	public WordLib() {
-		this.words = new String[]{"pearl", "government", "mess", "winner", "person", "superior", "deck", "engineer", "abundant", "irony", "utter", "archaic",
-				"dead", "premium", "permission", "attitude", "see", "baseball", "depart", "increase", "grimace", "manual", "teach", "match", "long", "delicate",
-				"vessel", "payment", "sink", "plead", "convince", "census", "conductor", "registration", "proper", "table", "pitch", "exchange", "landowner", "coup", "grace",
-				"growth", "earsplitting", "cabbageworm"};
+		f = new File("words.txt"); // new file for words.txt
+		num = 46; // set at 46 (number of words in f)
+		words = new String[num]; // num controls size of words[]
+		readWords(f); // calls readWords 
+	}
+	
+	/* Reads in words from File fi separated by new lines*/
+	private void readWords(File fi) {
+		try { 
+			sc = new Scanner(fi);
+			
+			for (int i = 0; i < num && sc.hasNext(); i++) {
+				words[i] = sc.next();
+			}
+			
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
 	}
 
 	public String[] getWords() {
